40009eaefcac35302b9a314ce7701aacdc550998
netohelvecio <helvecioneto77@gmail.com>
2019-12-28 14:00:07 -0300

    add horarios

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -19,7 +19,7 @@ import api from '~/services/api';
 
 import { Container, Time } from './styles';
 
-const range = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+const range = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
 
 export default function Dashboard() {
   const [schedule, setSchedule] = useState([]);
